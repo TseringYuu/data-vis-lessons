9f9a1f44c579d45aceb9cd0b3fe59e7028d7b930
前端鱼 <10785566+FE-Yuu@user.noreply.gitee.com>
2022-05-02 00:43:53 +0800

    echarts异常点标记

---
+++ echarts/异常点标记/data.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+        date: '2016-05-01',
+        value: 123,
+        hasError: false,
+    },
+    {
+        date: '2016-05-02',
+        value: 412,
+        hasError: true,
+    },
+    {
+        date: '2016-05-03',
+        value: 312,
+        hasError: false,
+    },
+    {
+        date: '2016-05-04',
+        value: 521,
+        hasError: false,
+    },
+    {
+        date: '2016-05-05',
+        value: 231,
+        hasError: false,
+    },
+];
